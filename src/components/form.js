2a19901b79b7f3d26924a00f6124989bd7ef00f8
Fernando Chavez <fjchavez01@gmail.com>
2020-10-16 10:19:01 -0700

    finished basic form

--- src/components/form.js
+++ src/components/form.js
@@ -20,11 +20,12 @@ background-color: #2C7773;
 `
 export default function Form(){
     const initialFormValues = {
+        name:'',
         size: '',
         sauce: '',
-        peperoni: false,
+        pepperoni: false,
         olives: false,
-        echeese: false,
+        cheese: false,
         sausage: false,
         gf: false,
         instructions: '',
@@ -46,6 +47,12 @@ export default function Form(){
         <Mdiv> 
             <p>Build Your Pizza</p>
             <form onSubmit={submit}>
+                <MLabel>
+                    <p>Name</p>
+                    <input type='text' value={formValues.name} onChange={change} name='name'/>
+                </MLabel>
+
+
                 <MLabel>
                   <p>Choice of size<br/>Required</p>
                    <Select name='size' value={formValues.size} onChange={change}>
@@ -72,7 +79,37 @@ export default function Form(){
                     </label>
                 </MLabel>
 
-                
+                <MLabel>
+                    <p>Add Toppings</p>
+                    <label htmlFor='pepperoni'> Pepperoni
+                        <input type='checkbox' name='pepperoni' value={true} id='pepperoni' onChange={change}/>
+                    </label>
+                    <label htmlFor='olives'> Olives
+                        <input type='checkbox' name='olives' value={true} id='olives' onChange={change}/>
+                    </label>
+                    <label htmlFor='cheese'> Extra Cheese
+                        <input type='checkbox' name='cheese' value={true} id='cheese' onChange={change}/>
+                    </label>
+                    <label htmlFor='sausage'> Sausage
+                        <input type='checkbox' name='sausage' value={true} id='sausage' onChange={change}/>
+                    </label>
+                </MLabel>
+
+                <MLabel>
+                    <p>Choice of substitute</p>
+                    <label htmlFor='gf'> Gluten Free Crust
+                        <input type='checkbox' name='gf' value={true} id='gf' onChange={change}/>
+                    </label>
+                </MLabel>
+
+                <MLabel>
+                    <p>Special instructions</p>
+                    <input type='text' value={formValues.instructions} onChange={change} name='instructions'/>
+                </MLabel>
+
+                <MLabel>
+                    <input type='submit' value='Place Order'/>
+                </MLabel>
             </form>
         </Mdiv>
     )
